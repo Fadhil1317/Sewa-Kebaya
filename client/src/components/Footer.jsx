e8acf55fbff106c3d61f2863d5e0bd40043be431
auliafiona2@gmail.com <auliafiona2@gmail.com>
2026-06-27 21:22:42 +0700

    memperbarui alamat

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import { Instagram } from "lucide-react";
+
 const Footer = () => {
   return (
     <footer className="bg-stone-950 text-stone-400 pt-16 pb-8 border-t border-amber-900/30 w-full mt-auto">
@@ -34,7 +35,7 @@ const Footer = () => {
             </div>
             <div className="flex items-center gap-4 text-stone-300">
               <Instagram className="text-amber-700" size={16} /> 
-                <a 
+              <a 
                 href="https://www.instagram.com/swasanakebya_?utm_source=qr" 
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -50,17 +51,19 @@ const Footer = () => {
         <div className="w-full h-44 rounded-2xl overflow-hidden grayscale contrast-125 opacity-40 hover:grayscale-0 hover:opacity-100 transition-all duration-700 border border-stone-800 shadow-2xl">
           <iframe 
             title="Lokasi Toko"
-            src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.2166597144083!2d109.09830697400508!3d-7.551337992462313!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e65693a1b6ce331%3A0x877ad99d4d43a940!2sRias%20pengantin%20daryatmi%20Larasati!5e0!3m2!1sid!2sid!4v1782569816859!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>" 
+            // DI SINI SUDAH DIPERBAIKI: Hanya mengambil isi link URL-nya saja
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.2166597144083!2d109.09830697400508!3d-7.551337992462313!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e65693a1b6ce331%3A0x877ad99d4d43a940!2sRias%20pengantin%20daryatmi%20Larasati!5e0!3m2!1sid!2sid!4v1782569816859!5m2!1sid!2sid" 
             className="w-full h-full"
             style={{ border: 0 }}
             allowFullScreen="" 
             loading="lazy"
+            referrerPolicy="strict-origin-when-cross-origin"
           ></iframe>
         </div>
       </div>
       
       <div className="mt-16 pt-8 border-t border-stone-900 text-center text-[9px] uppercase tracking-[0.5em] text-stone-700">
-        © {new Date().getFullYear()} KebayaKlasik UMKM • Selaras dalam Budaya
+        © {new Date().getFullYear()} SwasanaKebaya UMKM • Selaras dalam Budaya
       </div>
     </footer>
   );
